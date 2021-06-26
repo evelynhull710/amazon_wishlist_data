f6cc06540745cd71dc558d05d896c63f906cba3b
Evelyn Hull Valdez <evelyn.hull@EvelynHull.local>
2021-06-26 12:00:31 -0700

    kind of get a list of titles, lets take it from here, need a little bit of format

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const axios = require('axios');
 const fs = require('fs');
 const AmazonWishList = require('./amazon-wl-parser');
 const cheerio =require('cheerio');
+const jwt_decode = require('jwt-decode');
+
+const BASE_URL_MX='https://www.amazon.com.mx';
+const PUBLIC_ID='JQK192QOJWAT'
+const START_URL_WL=`/hz/wishlist/ls/${PUBLIC_ID}`;
 // var AmazonWishList = require('amazon-wish-list');
 // var awl = new AmazonWishList.default('JQK192QOJWAT');
  //https://github.com/stylesuxx/amazon-wish-list
@@ -16,42 +21,78 @@ const cheerio =require('cheerio');
 
 //is just different in mx 
 
-app.get('/wishlist', function (req, res) {
-   // Make a request for a user with a given ID
-    axios.get('https://www.amazon.com.mx/hz/wishlist/ls/JQK192QOJWAT',{
-        scrollY:1000
-      })
-    .then(function (response) {
-        console.log("response next: "+response.next);
-        const html = response.data;
-        const $ = cheerio.load(html);
+app.get('/wishlist', async function (req, res) {
+let nextPageUrl='';
+
+    let response=await axios.get(BASE_URL_MX+START_URL_WL);
+//showMoreUrl   <- input name that has next url
+        let html = response.data;
+        let $ = cheerio.load(html);
         let title=$('#profile-list-name').text();
         let itemTitles=$('h3 .a-link-normal').text();
-        let prices=$('.price-section').text();
+        let urlValues=$('.showMoreUrl').val();;
+        console.log(urlValues);
+        let prices=[];
+        let pageUrls=[];
+        $('.price-section').text().split('\n').forEach(element=>{
+            if(element.trim()!=''&&element.trim().startsWith('$')){
+                prices.push(element.trim());
+            }
+        });
+        
         console.log(prices);
-        const titles=[];
+        let titles=[];
         itemTitles.split('\n').forEach(element => {
             if(element.trim()!='')
             titles.push(element);
         });
         console.log(titles);
-        // handle success
-        fs.writeFile("test.txt", $.text(), function(err) {
-            if(err) {
-                return console.log(err);
+        // fs.writeFile('test.txt', titles.join('\n'), function (err) {
+        //     if (err) throw err;
+        //     console.log('Saved!');
+        //   });
+        let duplicated=false;
+        while(!duplicated){
+            nextPageUrl=urlValues;
+            if(pageUrls.includes(nextPageUrl)){duplicated=true;break;}
+            pageUrls.push(nextPageUrl);
+            let response= await axios.get(BASE_URL_MX+nextPageUrl);
+            html = response.data;
+                $ = cheerio.load(html);
+                title=$('#profile-list-name').text();
+                itemTitles=$('h3 .a-link-normal').text();
+                urlValues=$('.showMoreUrl').val();;
+                console.log(urlValues);
+                $('.price-section').text().split('\n').forEach(element=>{
+                    if(element.trim()!=''&&element.trim().startsWith('$')){
+                        prices.push(element.trim());
+                    }
+                });
+                
+                console.log(prices);
+                itemTitles.split('\n').forEach(element => {
+                    // if(titles.includes(element)){
+                    //     duplicated=true;
+                    // }
+                    if(element.trim()!=''&&!duplicated)
+                    titles.push(element);
+                });
+                console.log(titles);
+                // fs.appendFile('test.txt', titles.join('\n'), function (err) {
+                //     if (err) throw err;
+                //     console.log('Saved!');
+                //   });
+             //NEED TO FIX 503     
             }
-            console.log("The file was saved!");
-        }); 
+            console.log('got out from loop!');
+            // const set = new Set(titles);
+            // console.log('size:',set.size);
+            // fs.writeFile('test.txt', [...set].join('\n'),function(err){
+            //     if(err)throw err;
+            //     console.log('saved!')
+            // });
         
-        console.log(response);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .then(function () {
-        // always executed
-    });
+        //console.log(response);
 
     res.send('hello world')
 })
@@ -64,9 +105,6 @@ app.get('/', function (req, res) {
      res.send('hello world')
  })
 
-// awl.getById('3EGL33WZPAJ8Q').then(function(list) {
-//   console.log(list);
-// });
 
 
 app.listen(3000)
